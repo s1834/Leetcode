41d208cab495fffffae773717a2c8a8ebfb24115
s1834 <s18324@protonmail.com>
2023-07-27 01:50:06 +0530

    :rocket: Accepted

---
+++ Solved Problems/Easy/1773_Count_Items_Matching_a_Rule.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int count = 0, n = items.size();
+        for (int i = 0; i < n; i++) {
+            if ((ruleKey == "type" && items[i][0] == ruleValue) || (ruleKey == "color" && items[i][1] == ruleValue) || (ruleKey == "name" && items[i][2] == ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
