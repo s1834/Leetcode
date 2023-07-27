601d8e3cda40a965876d16896e897c8baf66a673
s1834 <s18324@protonmail.com>
2023-07-28 02:16:01 +0530

    :rocket: Accepted

--- Study Plan/Data Structure/Data Structure I/Day 1/53_Maximum_Subarray.cpp
+++ Study Plan/Data Structure/Data Structure I/Day 1/53_Maximum_Subarray.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max = nums[0], temp = 0, n = nums.size(), j = 0;
-        for (int i = j + 1; i < n; i++) {
-            temp = nums[j];
-            if (temp > max) {
-                max = temp;
-            }
+        int max = nums[0], temp = 0, n = nums.size();
+        for (int i = 0; i < n; i++) {
             temp += nums[i];
+            if (nums[i] > temp) {
+                temp = nums[i];
+            }
             if (temp > max) {
                 max = temp;
-            }
-            if (i == n - 1) {
-                j++;
-                i = j;
             }
         }
-        
 
         return max;
     }
