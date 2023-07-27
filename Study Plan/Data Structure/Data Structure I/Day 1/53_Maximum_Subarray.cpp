0b195944b76b396f37afaf151a133e406343b476
s1834 <s18324@protonmail.com>
2023-07-28 01:34:55 +0530

    :test_tube: Time Limit Exceeded

--- Study Plan/Data Structure/Data Structure I/Day 1/53_Maximum_Subarray.cpp
+++ Study Plan/Data Structure/Data Structure I/Day 1/53_Maximum_Subarray.cpp
@@ -4,6 +4,9 @@ public:
         int max = nums[0], temp = 0, n = nums.size();
         for (int i = 0; i < n; i++) {
             temp = nums[i];
+            if (temp > max) {
+                max = temp;
+            }
             for (int j = i + 1; j < n; j++) {
                 temp += nums[j];
                 if (temp > max) {
@@ -15,7 +18,7 @@ public:
         if (temp > max) {
              max = temp;
         }
-        
+
         return max;
     }
 };
