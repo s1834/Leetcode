a5083b016036fef9960bd4c22654b21620a216b3
s1834 <s18324@protonmail.com>
2023-07-31 01:25:14 +0530

    :test_tube: Wrong answer

--- Solved Problems/Easy/645_Set_Mismatch.cpp
+++ Solved Problems/Easy/645_Set_Mismatch.cpp
@@ -3,7 +3,13 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
+        if (nums[0] != 1) {
+            ans.push_back(nums[0]);
+            ans.push_back(1);
+            return ans;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
             if(nums[i] == nums[i + 1]) {
                 if (i > 0 && nums[i] != nums[i - 1] + 1) {
                     ans.push_back(nums[i]);
@@ -16,7 +22,7 @@ public:
                 }
             }
         }
-        
+
         return ans;
     }
 };
