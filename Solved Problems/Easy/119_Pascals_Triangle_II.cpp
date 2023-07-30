8b090f1a09d5c3cde32312abbde4a394b6c1ae04
s1834 <s18324@protonmail.com>
2023-07-30 19:42:04 +0530

    :zap: Improved runtime but degraded memory

--- Solved Problems/Easy/119_Pascals_Triangle_II.cpp
+++ Solved Problems/Easy/119_Pascals_Triangle_II.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> ans;
         for (int i = 0; i <= rowIndex; i++) {
-             vector<int> vect;
-            for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
-                    vect.push_back(1);
-                } else {
-                    vect.push_back(ans[i - 1][j] + ans[i - 1][j - 1]);
-                }
+             vector<int> vect(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                vect[j] = ans[i - 1][j] + ans[i - 1][j - 1];
             }
             ans.push_back(vect);
         }
