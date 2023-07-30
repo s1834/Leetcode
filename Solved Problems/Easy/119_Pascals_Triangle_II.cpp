f38a933be6a0f917a7772f66b859628189091d5b
s1834 <s18324@protonmail.com>
2023-07-30 19:36:04 +0530

    :poop: Tried to improve performance but decreased it

--- Solved Problems/Easy/119_Pascals_Triangle_II.cpp
+++ Solved Problems/Easy/119_Pascals_Triangle_II.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<vector<int>> ans;
-        vector<int> vect;
-
         for (int i = 0; i <= rowIndex; i++) {
+             vector<int> vect;
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
                     vect.push_back(1);
@@ -12,13 +11,9 @@ public:
                     vect.push_back(ans[i - 1][j] + ans[i - 1][j - 1]);
                 }
             }
-
             ans.push_back(vect);
-            if (i != rowIndex) {
-                vect.clear();
-            }
         }
 
-        return vect;
+        return ans[rowIndex];
     }
 };
