0d1f83564e6bcdbccf7452a88ec0edee6b8a03ec
s1834 <s18324@protonmail.com>
2023-08-01 21:15:37 +0530

    :test_tube: Wrong Answer

---
+++ Study Plan/Data Structure/Data Structure I/Day 6 (String)/383_Ransom_Note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char, int> mp1, mp2; 
+        int n1 = magazine.size(), n2 = ransomNote.size();
+        for (int i = 0; i < n1; i++) {
+            mp1[magazine[i]]++;
+        }
+        for (int i = 0; i < n2; i++) {
+            mp2[ransomNote[i]]++;
+        }
+        for (int i = 0; i < n1; i++) {
+            if (mp2[magazine[i]] != mp2[ransomNote[i]]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
