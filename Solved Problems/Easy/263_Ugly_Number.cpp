d8e33f1637cf06f275304b046c960b74ace22def
s1834 <s18324@protonmail.com>
2023-08-10 22:34:42 +0530

    :test_tube: Wrong Answer

---
+++ Solved Problems/Easy/263_Ugly_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0 && (i != 2 && i != 3 && i != 5)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
