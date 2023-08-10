15b818a5b463c374d3675aac3889127f7ffe0a27
s1834 <s18324@protonmail.com>
2023-08-10 22:39:34 +0530

    :test_tube: Wrong Answer

--- Solved Problems/Easy/263_Ugly_Number.cpp
+++ Solved Problems/Easy/263_Ugly_Number.cpp
@@ -11,10 +11,10 @@ public:
     }
 
     bool isUgly(int n) {
-        if (n < 0) {
+        if (n <= 0) {
             return false;
         }
-        
+
         for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0 && (i != 2 && i != 3 && i != 5)) {
                 if (isPrime(i)) {
