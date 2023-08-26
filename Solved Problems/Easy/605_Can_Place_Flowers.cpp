7441858a3a621d644c9fcbd2eb1c0a64da118234
s1834 <s18324@protonmail.com>
2023-08-27 01:48:21 +0530

    Runtime Error

--- Solved Problems/Easy/605_Can_Place_Flowers.cpp
+++ Solved Problems/Easy/605_Can_Place_Flowers.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        // int size = flowerbed.size(), count = 0;
-        // for(int i = 0; i < size; i++) {
-        //     if(flowerbed[i] == 0) {
-        //         count++;
-        //     }
-        // }
-
-        // if (count == 2 * n + 2) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-
-        int size = flowerbed.size(), flag = (flowerbed[0] == 1) ? 0 : 1;
+        int size = flowerbed.size();
         for (int i = 0; i < size; i++) {
-            if (flowerbed[i] != 1 && flowerbed[i + 1] != 1 && !flag) {
+            if (flowerbed[i] != 1 && flowerbed[i + 1] != 1 && flowerbed[i - 1] != 1) {
                 n--;
-                flag = 1;
-            } else {
-                flag = 0;
+                flowerbed[i] = 1;
             }
         }
 
