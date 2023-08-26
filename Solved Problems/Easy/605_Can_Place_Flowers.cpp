66cd1ea792279aae2fa720edb14b8b7053b675bd
s1834 <s18324@protonmail.com>
2023-08-27 02:07:55 +0530

    Accepted: needed help from editorial

--- Solved Problems/Easy/605_Can_Place_Flowers.cpp
+++ Solved Problems/Easy/605_Can_Place_Flowers.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int size = flowerbed.size();
-        if (size < 3) {
-            if(n == 0) {
-                return true;
-            } else if(n == 1 && flowerbed[0] == 0) {
-                if (size > 1) {
-                    if (flowerbed[1] == 0) {
+        int size = flowerbed.size(), count = 0;
+        for (int i = 0; i < size; i++) {
+            if (flowerbed[i] == 0) {
+                bool left = (i == 0) || (flowerbed[i - 1] == 0);
+                bool right = (i == size - 1) || (flowerbed[i + 1] == 0);
+
+                if (left && right) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) {
                         return true;
-                    } else {
-                        return false;
                     }
                 }
-                return true;
-            } else {
-                return false;
             }
         }
-
-        for (int i = 0; i < size; i++) {
-            if (i == 0 && flowerbed[i] != 1 && flowerbed[i + 1] != 1) {
-                n--;
-                flowerbed[i] = 1;
-            } else if (i == size - 1 && flowerbed[i] != 1 && flowerbed[i - 1] != 1) {
-                n--;
-                flowerbed[i] = 1;
-            } else if (flowerbed[i] != 1 && flowerbed[i + 1] != 1 && flowerbed[i - 1] != 1) {
-                n--;
-                flowerbed[i] = 1;
-            }
-        }
-
-        if (n) {
-            return false;
-        }
-        return true;
+        return count >= n;
     }
 };
