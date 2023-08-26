9a2f9118dc1eff43ac25851e8f28377139d59e42
s1834 <s18324@protonmail.com>
2023-08-27 02:00:00 +0530

    Runtime Error: at [0,0,1,0]

--- Solved Problems/Easy/605_Can_Place_Flowers.cpp
+++ Solved Problems/Easy/605_Can_Place_Flowers.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         int size = flowerbed.size();
-        if (size == 1) {
+        if (size < 3) {
             if(n == 0) {
                 return true;
             } else if(n == 1 && flowerbed[0] == 0) {
+                if (size > 1) {
+                    if (flowerbed[1] == 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
                 return true;
             } else {
                 return false;
