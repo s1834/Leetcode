71e1575ba708d95ef19a8b7c184afd2c3341dcf9
s1834 <s18324@protonmail.com>
2023-08-27 00:26:45 +0530

    Accepted: instead of checking for a particular char, check for vowels

--- Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
+++ Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int n = s.size();
-        stack<int> st;
-        string ans = "";
+        stack<char> st;
     
         for (int i = 0; i < n; i++) {
             if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
                 st.push(s[i]);
-                ans.push_back(' ');
-            } else {
-                ans.push_back(s[i]);
             }
         }
 
         for (int i = 0; i < n; i++) {
-            if (ans[i] == ' ') {
-                ans[i] = st.top();
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+                s[i] = st.top();
                 st.pop();
             }
         }
 
-        return ans;
+        return s;
     }
 };
