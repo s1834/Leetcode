62e3a0b1c27c0b4f09a9f901e282750426a06f63
s1834 <s18324@protonmail.com>
2023-08-27 00:18:49 +0530

    Wrong Answer: 0 testcase failed

--- Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
+++ Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
@@ -6,7 +6,7 @@ public:
         string ans = "";
     
         for (int i = 0; i < n; i++) {
-            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
                 st.push(s[i]);
                 ans.push_back('0');
             } else {
