f5f6933b87572990ee3da5499bcb350c16e98cdb
s1834 <s18324@protonmail.com>
2023-08-27 01:26:51 +0530

    Accepted: same as 724

---
+++ Solved Problems/Easy/1991_Find_the_Middle_Index_in_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n = nums.size(), sum = 0, leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
