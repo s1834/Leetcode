8afdcd85bf6867fe4b6e4977032df223aee17ea5
s1834 <s18324@protonmail.com>
2023-08-27 00:37:08 +0530

    Accepted: Learned vector erase func

---
+++ Solved Problems/Easy/283_Move_Zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), i = 0;
+        while (i < n) {
+            if (nums[i] == 0) {
+                nums.erase(nums.begin() + i);
+                nums.push_back(0);
+                n--;
+            } else {
+                i++;
+            }
+        }
+    }
+};
