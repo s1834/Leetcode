f186cedf6bff2566ddeb20a54933e494a290d1a3
s1834 <s18324@protonmail.com>
2023-08-27 00:20:14 +0530

    Wrong Answer: space testcase failed

--- Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
+++ Solved Problems/Easy/345_Reverse_Vowels_of_a_String.cpp
@@ -8,14 +8,14 @@ public:
         for (int i = 0; i < n; i++) {
             if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
                 st.push(s[i]);
-                ans.push_back('0');
+                ans.push_back(' ');
             } else {
                 ans.push_back(s[i]);
             }
         }
 
         for (int i = 0; i < n; i++) {
-            if (ans[i] == '0') {
+            if (ans[i] == ' ') {
                 ans[i] = st.top();
                 st.pop();
             }
