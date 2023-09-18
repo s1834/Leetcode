f267ca4b364583121cd88a2e7f7d547818fc1112
s1834 <s18324@protonmail.com>
2023-09-19 01:27:45 +0530

    Time Limit Exceeded

--- Solved Problems/Easy/643_Maximum_Average_Subarray_I.cpp
+++ Solved Problems/Easy/643_Maximum_Average_Subarray_I.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         double ans = INT_MIN, temp = 0;
-        int n = nums.size();
-        for (int i = 0; i < n - k + 1; i++) {
+        int n = nums.size(), count = 0, j = 0;
+        for (int i = j; i < n; i++) {
             temp += nums[i];
-            for (int j = i + 1; j < n && j < i + k; j++) {
-                temp += nums[j];
+            count++;
+            if (count == k) {
+                temp /= k;
+                if (temp > ans) {
+                    ans = temp;
+                }
+                j++;
+                i = j - 1;
+                count = 0;
+                temp = 0;
             }
-            temp /= k;
-            if (temp > ans) {
-                ans = temp;
-            }
-            temp = 0;
         }
 
         return ans;
