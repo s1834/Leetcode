0577956eb59cb9098c6523acf49d9db64ba3b8ea
s1834 <s18324@protonmail.com>
2023-09-19 01:10:19 +0530

    Wrong Answer on [-1], 1

---
+++ Solved Problems/Easy/643_Maximum_Average_Subarray_I.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double ans = 0, temp = 0;
+        int n = nums.size();
+        for (int i = 0; i < n - k + 1; i++) {
+            temp += nums[i];
+            for (int j = i + 1; j < n && j < i + k; j++) {
+                temp += nums[j];
+            }
+            temp /= k;
+            if (temp > ans) {
+                ans = temp;
+            }
+            temp = 0;
+        }
+
+        return ans;
+    }
+};
