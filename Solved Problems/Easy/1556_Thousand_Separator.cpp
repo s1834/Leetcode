4b331826b0354d62b8aeb49b3deb09d2263e46e1
s1834 <s18324@protonmail.com>
2023-09-28 18:00:11 +0530

    Accepted: added testcase for 0

--- Solved Problems/Easy/1556_Thousand_Separator.cpp
+++ Solved Problems/Easy/1556_Thousand_Separator.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     string thousandSeparator(int n) {
+        if (n == 0) {
+            return "0";
+        } 
+        
         string s = "";
         int count = 0;
         while (n) {
