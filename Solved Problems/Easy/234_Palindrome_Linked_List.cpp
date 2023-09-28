f9d53812ff6a522980e15cba11a1e16199268392
s1834 <s18324@protonmail.com>
2023-09-28 15:42:32 +0530

    Wrong Answer: [1,0,1], ig issue with odd number

---
+++ Solved Problems/Easy/234_Palindrome_Linked_List.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head) {
+            return 0;
+        }
+        
+        int count = 0;
+        ListNode* ptr = head;
+        while (ptr) {
+            count++;
+            ptr = ptr->next;
+        }
+        ptr = head;
+        count /= 2;
+        ListNode *prev = NULL, *cur = head, *nxt = cur->next;
+        while (count) {
+            cur->next = prev;
+            prev = cur;
+            cur = nxt;
+            nxt = cur->next;
+            count--;
+        }
+        
+        while (prev && cur) {
+            if (prev->val != cur->val) {
+                return false;
+            }
+            prev = prev->next;
+            cur = cur->next;
+        }
+
+        return true;
+    }
+};
