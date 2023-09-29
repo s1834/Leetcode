a7f42af9d1c0294a266950bbda6b64e69ccaba78
s1834 <s18324@protonmail.com>
2023-09-29 13:37:03 +0530

    Runtime Error: on [1,2,3,4,5,6,7,8]

---
+++ Solved Problems/Medium/328_Odd_Even_Linked_List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL) {
+            return NULL;
+        }
+        ListNode* odd = head, *even = head->next, *join = head->next;
+        while (odd && even) {
+            odd->next = even->next;
+            odd = even->next;
+            even->next = odd->next;
+            even = odd->next;
+        }
+        odd->next = join;
+        return head;
+    }
+};
