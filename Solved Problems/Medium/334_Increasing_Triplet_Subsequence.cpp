4f53508e5d09d639d1200e130c52887009acb3c2
s1834 <s18324@protonmail.com>
2023-09-29 08:51:23 +0530

    Time Limit Exceeded

--- Solved Problems/Medium/334_Increasing_Triplet_Subsequence.cpp
+++ Solved Problems/Medium/334_Increasing_Triplet_Subsequence.cpp
@@ -7,8 +7,14 @@ public:
         }
 
         for (int i = 0; i < n - 2; i++) {
-            if (nums[i] < nums[i + 1] && nums[i + 1] < nums[i + 2]) {
-                return true;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (nums[i] < nums[j]) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (nums[j] < nums[k]) {
+                            return true;
+                        }
+                    }
+                }
             }
         }
 
