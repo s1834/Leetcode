62b6f7945ac1875663cb6a9b78c08ea490ee489c
s1834 <s18324@protonmail.com>
2023-09-30 11:50:28 +0530

    Wrong Answer: on 4

--- Solved Problems/Easy/367_Valid_Perfect_Square.cpp
+++ Solved Problems/Easy/367_Valid_Perfect_Square.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
+        if (num == 1) {
+            return true;
+        }
         int n = num / 2, temp;
         for (int i = 1; i < n; i++) {
             temp = i * i;
