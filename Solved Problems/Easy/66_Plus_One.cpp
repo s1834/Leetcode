bb2047b12182f3221eccf61c9f98893ba55f9118
s1834 <s18324@protonmail.com>
2023-10-01 21:27:57 +0530

    Runtime Error

---
+++ Solved Problems/Easy/66_Plus_One.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size(), ten = n - 1, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += digits[i] * pow(10, ten);
+            ten--;
+        }
+        sum++;
+        vector<int> vec;
+        while(sum) {
+            vec.push_back(sum % 10);
+            sum /= 10;
+        }
+        reverse(vec.begin(), vec.end());
+        return vec;
+    }
+};
