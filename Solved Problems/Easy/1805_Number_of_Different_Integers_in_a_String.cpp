93bbe2c6dc45987ed0ac2c6cf2cd1f3faafe0ee5
s1834 <s18324@protonmail.com>
2023-10-02 01:16:18 +0530

    Wrong Answer: zero answer solved

--- Solved Problems/Easy/1805_Number_of_Different_Integers_in_a_String.cpp
+++ Solved Problems/Easy/1805_Number_of_Different_Integers_in_a_String.cpp
@@ -3,24 +3,30 @@ public:
     int numDifferentIntegers(string word) {
         set<string> s1;
         string temp = "";
-        int count = 0;
+        int count = 0, zero = 0;
         int n = word.size();
         for (int i = 0; i < n; i++) {
             if (isdigit(word[i])) {
                 if (!count && word[i] == '0') {
+                    zero = 1;
                     continue;
                 }
                 temp += word[i];
                 count++;
+                zero = 0;
             }
             if (!isdigit(word[i]) && temp != "") {
                 s1.insert(temp);
                 temp = "";
                 count = 0;
+                zero = 0;
             }
         }
         if (temp != "") {
             s1.insert(temp);
+        }
+        if (zero) {
+            s1.insert("0");
         }
         return s1.size();
     }
