de43e93679373ed3b66c838c352ec554aa32a4e7
s1834 <s18324@protonmail.com>
2023-10-02 01:13:36 +0530

    Wrong Answer: previous issue was solved about integer shit, we didnt need to convert string to integer

--- Solved Problems/Easy/1805_Number_of_Different_Integers_in_a_String.cpp
+++ Solved Problems/Easy/1805_Number_of_Different_Integers_in_a_String.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int numDifferentIntegers(string word) {
-        set<char> s1;
+        set<string> s1;
         string temp = "";
+        int count = 0;
         int n = word.size();
         for (int i = 0; i < n; i++) {
             if (isdigit(word[i])) {
+                if (!count && word[i] == '0') {
+                    continue;
+                }
                 temp += word[i];
+                count++;
             }
             if (!isdigit(word[i]) && temp != "") {
-                s1.insert(stoi(temp));
+                s1.insert(temp);
                 temp = "";
+                count = 0;
             }
         }
         if (temp != "") {
-            s1.insert(stoi(temp));
+            s1.insert(temp);
         }
         return s1.size();
     }
