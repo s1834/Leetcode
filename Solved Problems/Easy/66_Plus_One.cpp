44d20cfb72559c2b87d7169edad0e015d1fe878a
s1834 <s18324@protonmail.com>
2023-10-01 21:44:46 +0530

    Accepted: Needed to change value of carry from ++ to 1

--- Solved Problems/Easy/66_Plus_One.cpp
+++ Solved Problems/Easy/66_Plus_One.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size(), ten = n - 1;
-        long long int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += digits[i] * pow(10, ten);
-            ten--;
-        }
-        sum++;
-        vector<int> vec;
-        while(sum) {
-            vec.push_back(sum % 10);
-            sum /= 1class Solution {
-public:
-    vector<int> plusOne(vector<int>& digits) {
         reverse(digits.begin(), digits.end());
         int n = digits.size(), carry = 1;
         for (int i = 0; i < n; i++) {
@@ -22,7 +9,7 @@ public:
                 digits.push_back(1);
             } else if (digits[i] == 9) {
                 digits[i] = 0;
-                carry++;
+                carry = 1;
             } else if (carry) {
                 digits[i] += carry;
                 break;
@@ -30,10 +17,5 @@ public:
         }
         reverse(digits.begin(), digits.end());
         return digits;
-    }
-};0;
-        }
-        reverse(vec.begin(), vec.end());
-        return vec;
     }
 };
