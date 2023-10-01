42204af95b3583def6984737c52304fd341b286b
s1834 <s18324@protonmail.com>
2023-10-02 02:37:08 +0530

    Wrong Answer: on 36/40

---
+++ Solved Problems/Easy/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int n = sentence.size(), n1 = searchWord.size(), count = 0, flag;
+        for (int i = 0; i < n; i++) {
+            flag = 1;
+            if (count == 0 || sentence[i] == ' ') {
+                for (int j = 0; j < n1; j++) {
+                    if (sentence[i + j + 1] == ' ' || sentence[i + j + 1] != searchWord[j]) {
+                        flag = 0;
+                        break;
+                    }
+                }
+                count++;
+                if(flag) {
+                    return count;
+                }
+            }
+        }
+        return -1;
+    }
+};
