f175d4d458c2f10c34ede3b91e8981a0037e5f31
s1834 <s18324@protonmail.com>
2023-10-02 02:43:19 +0530

    Accepted: Added cases if i == 0

--- Solved Problems/Easy/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
+++ Solved Problems/Easy/1455_Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.cpp
@@ -4,8 +4,15 @@ public:
         int n = sentence.size(), n1 = searchWord.size(), count = 0, flag;
         for (int i = 0; i < n; i++) {
             flag = 1;
-            if (count == 0 || sentence[i] == ' ') {
+            if (i == 0 || sentence[i] == ' ') {
                 for (int j = 0; j < n1; j++) {
+                    if(i == 0) {
+                        if(sentence[i + j] != searchWord[j]) {
+                            flag = 0;
+                            break;
+                        }
+                        continue;
+                    }
                     if (sentence[i + j + 1] == ' ' || sentence[i + j + 1] != searchWord[j]) {
                         flag = 0;
                         break;
