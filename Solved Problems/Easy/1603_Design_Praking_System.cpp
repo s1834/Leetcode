97ae772ff51517a300dfc7e6c850857d6d1e9a97
s1834 <s18324@protonmail.com>
2023-10-01 22:01:39 +0530

    Wrong Answer

---
+++ Solved Problems/Easy/1603_Design_Praking_System.cpp
@@ -0,0 +1,31 @@
+class ParkingSystem {
+private:
+    int b, m, s;
+public:
+    ParkingSystem(int big, int medium, int small) {
+        b = big;
+        m = medium;
+        s = small;
+    }
+    
+    bool addCar(int carType) {
+        if (carType == 1 && b) {
+            b--;
+            return true;
+        } else if (carType == 2 && m) {
+            m--;
+            return true;
+        } else if (s) {
+            s--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
