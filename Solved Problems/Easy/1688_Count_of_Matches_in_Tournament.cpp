5c8ecc09d3448ff3803007980510e839295c7c15
s1834 <s18324@protonmail.com>
2023-10-02 02:24:34 +0530

    Accepted

---
+++ Solved Problems/Easy/1688_Count_of_Matches_in_Tournament.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int count = 0;
+        while (n > 1) {
+            if (n % 2) {
+                n = (n - 1) / 2 + 1;
+                count += n - 1;
+            } else {
+                n /= 2;
+                count += n;
+            }
+        }
+        return count;
+    }
+};
