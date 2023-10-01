3d9fb0a4cb37cb4ec0ffbcc2db042b27a328a71a
s1834 <s18324@protonmail.com>
2023-10-01 13:28:58 +0530

    Accepted: The trick was to use doubly-linked list

--- Solved Problems/Medium/1472_Design_Browser_History.cpp
+++ Solved Problems/Medium/1472_Design_Browser_History.cpp
@@ -3,10 +3,10 @@ private:
     struct ListNode {
         string val;
         ListNode *next;
-        ListNode(string x) : val(x), next(NULL) {}
+        ListNode *prev;
+        ListNode(string x) : val(x), next(nullptr), prev(nullptr) {}
     };
-    ListNode* head, *ptr, *cur;
-    int x = 1, x1 = x, flag = 1;
+    ListNode* head, *ptr;
 public:
     BrowserHistory(string homepage) {
         head = new ListNode(homepage);
@@ -15,65 +15,26 @@ public:
     
     void visit(string url) {
         ListNode* newURL = new ListNode(url);
-        if (!flag) {
-            ptr = cur;
-        }
         ptr->next = newURL;
+        newURL->prev = ptr;
         ptr = newURL;
-        x++;
-        if (flag) {
-            x1 = x;
-            
-        } else {
-            cur = ptr;
-        }
     }
     
     string back(int steps) {
-        // int temp;
-        // flag = 0;
-        // if (x1 > steps && x1 - steps <= x) {
-        //     temp = x1 - steps - 1;
-        //     x1 -= steps;
-        // } else {
-        //     temp = 0;
-        //     x1 = 1;
-        // }
-        ListNode* move = cur;
-        flag = 0; 
-        int count = 0;
-        while(move) {
-            count++;
-            move = move->next;
+        ListNode* move = ptr;
+        while (steps-- && move->prev) {
+            move = move->prev;
         }
-        int temp = x - count - steps;
-        move = head;
-        while (temp && move->next) {
-            move = move->next;
-            temp--;
-        }
-        cur = move;
+        ptr = move;
         return move->val;
     }
     
     string forward(int steps) {
-        // int temp;
-        // flag = 0;
-        // if (x1 > steps && x1 + steps <= x) {
-        //     temp = x1 - steps - 1;
-        //     x1 += steps;
-        // } else {
-        //     temp = x1 - 1;
-        //     x1 = x;
-        // }
-        x1 += steps;
-        flag = 0;
-        ListNode* move = cur;
-        while (steps && move->next) {
+        ListNode* move = ptr;
+        while (steps-- && move->next) {
             move = move->next;
-            steps--;
         }
-        cur = move;
+        ptr = move;
         return move->val;
     }
 };
