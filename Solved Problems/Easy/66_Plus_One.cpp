7678abc81a45ad345afa60a3d0ae02e9ff0be6a7
s1834 <s18324@protonmail.com>
2023-10-01 21:43:01 +0530

    Wrong Answer: needed help from discusstions

--- Solved Problems/Easy/66_Plus_One.cpp
+++ Solved Problems/Easy/66_Plus_One.cpp
@@ -11,7 +11,27 @@ public:
         vector<int> vec;
         while(sum) {
             vec.push_back(sum % 10);
-            sum /= 10;
+            sum /= 1class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        reverse(digits.begin(), digits.end());
+        int n = digits.size(), carry = 1;
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1 && digits[i] == 9) {
+                digits[i] = 0;
+                digits.push_back(1);
+            } else if (digits[i] == 9) {
+                digits[i] = 0;
+                carry++;
+            } else if (carry) {
+                digits[i] += carry;
+                break;
+            }
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};0;
         }
         reverse(vec.begin(), vec.end());
         return vec;
