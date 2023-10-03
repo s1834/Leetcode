208d5b03431e6176a90bf22e6e67f063bee7019e
s1834 <s18324@protonmail.com>
2023-10-04 02:16:52 +0530

    Accepted: better runtime and memory than previous, still memory could be improved, needed helped from solutions, the trick was that nums[i] are between 1 and n so there are every number from 1 to n, so you could easily include that in a vector

--- Solved Problems/Medium/442_Find_All_Duplicates_in_an_Array.cpp
+++ Solved Problems/Medium/442_Find_All_Duplicates_in_an_Array.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        map<int, int> mp;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-        }
+        // v1
+        // map<int, int> mp;
+        // int n = nums.size();
+        // for (int i = 0; i < n; i++) {
+        //     mp[nums[i]]++;
+        // }
 
-        vector<int> vec;
-        for (auto x : mp) {
-            if (x.second == 2) {
-                vec.push_back(x.first);
+        // vector<int> vec;
+        // for (auto x : mp) {
+        //     if (x.second == 2) {
+        //         vec.push_back(x.first);
+        //     }
+        // }
+
+        // return vec;
+
+
+        // v2
+        vector<int> vec(nums.size(), 0), ans;
+        for (int i = 0; i < nums.size(); i++) {
+            if (vec[nums[i] - 1] != 0) {
+                ans.push_back(nums[i]);
+            } else {
+                vec[nums[i] - 1] = nums[i];
             }
-        }
-
-        return vec;
+        } 
+        return ans;
     }
 };
