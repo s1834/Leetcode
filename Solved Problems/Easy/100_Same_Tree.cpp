65179d32e12c021d0970c0d67db73edb52cad6ba
s1834 <s18324@protonmail.com>
2023-10-17 17:57:53 +0530

    Compile Error: Submitted by mistake

--- Solved Problems/Easy/100_Same_Tree.cpp
+++ Solved Problems/Easy/100_Same_Tree.cpp
@@ -12,19 +12,26 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(!p || !q) {
-            return true;
+        queue<TreeNode *> que1, que2;
+        que1.push(p->val);
+        que2.push(q->val);
+        while (!que1.empty() && !que2.empty()) {
+            TreeNode* front1 = que1.front(), *front2 = que2.front2();
+            que1.pop();
+            que2.pop();
+            if (front1->val != front2->val) {
+                return false;
+            }
+            if (front1->left != NULL) {
+                que1.push(front1->left);
+            }
+            if (front1->left != NULL) {
+                que1.push(front1->left);
+            }
+            if (front1->left != NULL) {
+                que1.push(front1->left);
+            }
+            
         }
-        if (p->val != q->val) {
-            return false;
-        }
-        if (!) {
-            return true;
-        }
-        if (!isSameTree(p->right,q->right)) {
-            return true;
-        }
-
-        return true;
     }
 };
