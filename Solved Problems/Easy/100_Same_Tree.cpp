0c8e6224734e8ba8445d073e1efe38b7e2aefe72
s1834 <s18324@protonmail.com>
2023-10-17 18:14:58 +0530

    Runtime Error

--- Solved Problems/Easy/100_Same_Tree.cpp
+++ Solved Problems/Easy/100_Same_Tree.cpp
@@ -12,26 +12,37 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) {
+            return true;
+        }
         queue<TreeNode *> que1, que2;
-        que1.push(p->val);
-        que2.push(q->val);
+        que1.push(p);
+        que2.push(q);
         while (!que1.empty() && !que2.empty()) {
-            TreeNode* front1 = que1.front(), *front2 = que2.front2();
+            TreeNode* front1 = que1.front(), *front2 = que2.front();
             que1.pop();
             que2.pop();
             if (front1->val != front2->val) {
                 return false;
             }
             if (front1->left != NULL) {
-                que1.push(front1->left);
-            }
-            if (front1->left != NULL) {
-                que1.push(front1->left);
-            }
-            if (front1->left != NULL) {
+                if (front2->left != NULL) {
+                    que2.push(front2->left);
+                } else {
+                    return false;
+                }
                 que1.push(front1->left);
             }
             
+            if (front1->right != NULL) {
+                if (front2->right != NULL) {
+                    que2.push(front2->right);
+                } else {
+                    return false;
+                }
+                que1.push(front1->right);
+            }       
         }
+        return true;
     }
 };
