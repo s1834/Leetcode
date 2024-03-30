7705cea853f383c71aa8d1a31d715583481864d4
s1834 <s18324@protonmail.com>
2024-03-30 20:48:55 +0530

    WA

--- Contest/Biweekly-Contest/127/100272_Shortest_Subarray_With_OR_at_Least_K_I.cpp
+++ Contest/Biweekly-Contest/127/100272_Shortest_Subarray_With_OR_at_Least_K_I.cpp
@@ -3,21 +3,23 @@ public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size(), temp = 0, sum = 0, tempCount = 0, count = INT_MAX;
         for (int i = 0; i < n; i++) {
-            temp |= nums[i];
-            tempCount++;
-            if(temp >= k) {
-                if(count > tempCount) {
-                    count = tempCount;
+            for (int j = i; j < n; j++) {
+                temp |= nums[j];
+                tempCount++;
+                if(temp >= k) {
+                    if(count > tempCount) {
+                        count = tempCount;
+                    }
+                    sum = temp;
+                    temp = 0;
+                    tempCount = 0;
                 }
-                sum = temp;
-                temp = 0;
-                tempCount = 0;
-            }
-            if(nums[i] >= k) {
-                temp = nums[i];
-                count = 1;
-                temp = 0;
-                tempCount = 0;
+                if(nums[j]  >= k) {
+                    sum = nums[j];
+                    count = 1;
+                    temp = 0;
+                    tempCount = 0;
+                }
             }
         }
         if(sum >= k) {
