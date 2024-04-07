1c87658348a6627cf8d2caf3abff49ef796d867d
s1834 <s18324@protonmail.com>
2024-04-07 14:50:24 +0530

    Runtime Error

---
+++ Solved Problems/Medium/2090_K_Radius_Subarray_Averages.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        vector<int> ans;
+        int n = nums.size(), sum = 0;
+        if(n < k * 2 + 1) {
+            for (int i = 0; i < n; i++) {
+                ans.push_back(-1);
+            }
+            return ans;
+        }
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+            ans.push_back(-1);
+        }
+
+        for (int i = k; i < k + k; i++) {
+            sum += nums[i];
+        }
+
+        for (int i = k; i < n - k; i++) {
+            sum += nums[i + k];
+            ans.push_back(round(sum / (k + k + 1)));
+            sum -= nums[i - k];
+        }
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(-1);
+        }
+
+        return ans;
+    }
+};
