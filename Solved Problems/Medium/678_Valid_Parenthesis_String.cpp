a9a89b60d07dd351a1959a1826f59f3d80d0161e
s1834 <s18324@protonmail.com>
2024-04-08 01:14:41 +0530

    Accepted: instead of char, take int in stack

--- Solved Problems/Medium/678_Valid_Parenthesis_String.cpp
+++ Solved Problems/Medium/678_Valid_Parenthesis_String.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<char> st1, st2;
+        stack<int> st1, st2;
         int n = s.size();
         for(int i = 0; i < n; i++) {
             if(s[i] == '(') {
-                st1.push('(');
+                st1.push(i);
             } else if (s[i] == '*') {
-                st2.push('*');
+                st2.push(i);
             } else {
-                if(st1.empty() && st2.empty()) {
-                    return false;
-                }
-                if(!st1.empty() && st1.top() == '(') {
+                if(!st1.empty()) {
                     st1.pop();
                 } else if(!st2.empty()) {
                     st2.pop();
@@ -23,8 +20,8 @@ public:
         }
 
         while(!st1.empty() && !st2.empty()) {
-            if(st1.top() == ')') {
-                break;
+            if (st1.top() > st2.top()) {
+                return false;
             }
             st1.pop();
             st2.pop();
