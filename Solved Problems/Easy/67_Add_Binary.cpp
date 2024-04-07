108d694976eb817eb31d8cc54dbbb014f5db9b2c
s1834 <s18324@protonmail.com>
2024-04-07 06:29:47 +0530

    WA

---
+++ Solved Problems/Easy/67_Add_Binary.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+
+        int n1 = a.size(), n2 = b.size();
+        int n = n1 < n2 ? n1 : n2;
+        string ans = "";
+        char carry = '0';
+        for(int i = 0; i < n; i++) {
+            if(a[i] == '1' && b[i] == '1') {
+                if(carry == '1') {
+                    ans += '1';
+                } else {
+                    ans += '0';
+                    carry = '1';
+                }
+            } else if((a[i] == '1' && b[i] == '0') || (a[i] == '0' && b[i] == '1')) {
+                if(carry == '1') {
+                    ans += '0';
+                } else {
+                    ans += '1';
+                }
+            } else {
+                if(carry == '1') {
+                    ans += '1';
+                    carry = '0';
+                } else {
+                    ans += '0';
+                }
+            }
+        }
+        int size;
+        if(n1 > n2) {
+            size = n1 - n;
+        } else {
+            size = n2 - n;
+            a = b;
+        }
+        for (int i = n - 1; i < size; i++) {
+            if(a[i] == '1' && carry == '1') {
+                ans += '0';
+            } else if ((a[i] == '1' && carry == '0') || (a[i] == '0' && carry == '1')) {
+                ans += '1';
+            } else {
+                ans += '0';
+            }
+        }
+        if(carry == '1') {
+            ans += '1';
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
