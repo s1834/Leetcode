c3afde8f17fbdb8efeaba4b756a17ee86767b373
s1834 <s18324@protonmail.com>
2024-04-08 08:04:23 +0530

    added extra if which was returning k, thats why WA

--- Solved Problems/Easy/2073_Time_Needed_to_Buy_Tickets.cpp
+++ Solved Problems/Easy/2073_Time_Needed_to_Buy_Tickets.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int n = tickets.size(), time = tickets[k];
-        if(tickets[k] == 1) {
-            return k;
-        }
         for(int i = 0; i < n; i++) {
             if(i < k) {
                 time += tickets[i] < tickets[k] ? tickets[i] : tickets[k];
