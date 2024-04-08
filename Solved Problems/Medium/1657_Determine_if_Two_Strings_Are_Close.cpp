047e72c66a0f21ab7e8b1d1fb7a189fc98cb6aa4
s1834 <s18324@protonmail.com>
2024-04-08 06:11:21 +0530

    Accepted: Using sets was removing dublicates, thus giving WA

--- Solved Problems/Medium/1657_Determine_if_Two_Strings_Are_Close.cpp
+++ Solved Problems/Medium/1657_Determine_if_Two_Strings_Are_Close.cpp
@@ -7,11 +7,14 @@ public:
             return false;
         }
 
+        set<char> s1, s2;
         for (int i = 0; i < n1; i++) {
             mp1[word1[i]]++;
+            s1.insert(word1[i]);
         }
         for (int i = 0; i < n2; i++) {
             mp2[word2[i]]++;
+            s2.insert(word2[i]);
         }
 
         if(mp1.size() != mp2.size()) {
@@ -24,29 +27,31 @@ public:
             }
         }
 
-        set<int> st1, st2;
-        n1 = mp1.size();
+        vector<int> v1, v2;
+        auto it1 = s1.begin(), it2 = s2.begin();
+        n1 = s1.size();
         for (int i = 0; i < n1; i++) {
-            cout << mp1[word1[i]]<<endl;
-            st1.insert(mp1[word1[i]]);
+            v1.push_back(mp1[*it1]);
+            it1++;
         }
+        n2 = s2.size();
         for (int i = 0; i < n2; i++) {
-            st2.insert(mp2[word2[i]]);
+            v2.push_back(mp2[*it2]);
+            it2++;
         }
 
-        n1 = st1.size();
-        n2 = st2.size();
+        n1 = v1.size();
+        n2 = v2.size();
         if(n1 != n2) {
             return false;
         }
 
-        auto it1 = st1.begin(), it2 = st2.begin();
+        sort(v1.begin(), v1.end());
+        sort(v2.begin(), v2.end());
         for (int i = 0; i < n1; i++) {
-            if(*it1 != *it2) {
+            if(v1[i] != v2[i]) {
                 return false;
             }
-            it1++;
-            it2++;
         }
         
         return true;
