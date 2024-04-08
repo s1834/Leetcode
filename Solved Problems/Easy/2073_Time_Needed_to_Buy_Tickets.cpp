51e191b3ec13eaddb24b52599f7ad5668636ee5d
s1834 <s18324@protonmail.com>
2024-04-08 08:01:51 +0530

    WA

--- Solved Problems/Easy/2073_Time_Needed_to_Buy_Tickets.cpp
+++ Solved Problems/Easy/2073_Time_Needed_to_Buy_Tickets.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
-        int n = tickets.size(), time = 0;
+        int n = tickets.size(), time = tickets[k];
         if(tickets[k] == 1) {
             return k;
         }
         for(int i = 0; i < n; i++) {
-            if(i != k) {
+            if(i < k) {
                 time += tickets[i] < tickets[k] ? tickets[i] : tickets[k];
+            } else if(i > k) {
+                time += tickets[i] < tickets[k] ? tickets[i] : tickets[k] - 1;
             }
         }
 
-        return time + tickets[k];
+        return time;
     }
 };
