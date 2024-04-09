b98d6c501343c303dcdcad84204f758279f3370e
s1834 <s18324@protonmail.com>
2024-04-09 09:17:19 +0530

    Accepted: with recursion

--- Solved Problems/Easy/342_Power_of_Four.cpp
+++ Solved Problems/Easy/342_Power_of_Four.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        if (n < 1) {
+            return false;
+        }
         if (n == 1) {
             return true;
         }
-
-        while (n >= 4) {
-            if (n == 4) {
-                return true;
-            }
-
-            if (n % 4 != 0) {
-                return false;
-            }
-            n /= 4;
-        }
-        return false;
+        return (n % 4 == 0 && isPowerOfFour(n / 4));
     }
 };
