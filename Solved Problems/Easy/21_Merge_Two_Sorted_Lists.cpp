f1edb7c068bd4606e288d47777475fb1767935cc
s1834 <s18324@protonmail.com>
2024-04-09 09:53:07 +0530

    Accepted: with recursion

--- Solved Problems/Easy/21_Merge_Two_Sorted_Lists.cpp
+++ Solved Problems/Easy/21_Merge_Two_Sorted_Lists.cpp
@@ -11,40 +11,23 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL) {
+        if(!list1) {
             return list2;
         }
-        if (list2 == NULL) {
+        if(!list2) {
             return list1;
         }
 
-        ListNode *mlist = list1;
-        if (list1->val > list2->val) {
-            mlist = list2;
-            list2 = list2->next;
+        ListNode* ptr;
+        if(list1->val <= list2->val) {
+            ptr = list1->next;
+            list1->next = mergeTwoLists(ptr, list2);;
         } else {
-            list1 = list1->next;
+            ptr = list1;
+            list1 = list2;
+            list1->next = mergeTwoLists(ptr, list1->next);
         }
-
-        ListNode *temp = mlist;
-        while (list1 && list2) {
-            if(list1->val < list2->val) {
-                temp->next = list1;
-                list1 = list1->next;
-            } else {
-                temp->next = list2;
-                list2 = list2->next;
-            }
-
-            temp = temp->next;
-        }
-
-        if(!list1) {
-            temp->next = list2;
-        } else {
-            temp->next = list1;
-        }
-
-        return mlist;
+        
+        return list1;
     }
 };
