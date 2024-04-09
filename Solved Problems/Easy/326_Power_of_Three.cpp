10d26e2685ad0674a64b35db59e6b5710fee524b
s1834 <s18324@protonmail.com>
2024-04-09 09:10:47 +0530

    Accepted: without recursion

---
+++ Solved Problems/Easy/326_Power_of_Three.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+
+        while (n >= 3) {
+            if (n == 3) {
+                return true;
+            }
+
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return false;
+    }
+};
