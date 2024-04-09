ac5d8251870677802b77518a8c7db3b94aca7d79
s1834 <s18324@protonmail.com>
2024-04-09 09:06:44 +0530

    Accepted: with recursion

--- Solved Problems/Easy/231_Power_of_Two.cpp
+++ Solved Problems/Easy/231_Power_of_Two.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        if (n < 1) {
+            return false;
+        }
         if (n == 1) {
             return true;
         }
-
-        while (n >= 2) {
-            if (n == 2) {
-                return true;
-            }
-
-            if (n % 2 != 0) {
-                return false;
-            }
-            n /= 2;
-        }
-        return false;
+        return (n % 2 == 0 && isPowerOfTwo(n / 2));
     }
 };
