7e4d1a9599b4c565b097f4bf2cb4cc8a17ee5c87
s1834 <s18324@protonmail.com>
2024-04-12 01:30:36 +0530

    Accepted: used binary search and recursion but it could still get better

--- Solved Problems/Medium/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
+++ Solved Problems/Medium/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        vector<int> indices(2);
-        
-        for (int i = 0; i < numbers.size(); i++) {
-            for (int j = i + 1; j < numbers.size(); j++) {
-                if(numbers[i] + numbers[j] == target) {
-                    indices[0] = i + 1;
-                    indices[1] = j + 1;
-                    i = numbers.size();
-                    break;
-                }
-            }
+    vector<int> ans = {-1, -1};
+
+    vector<int> findSum(vector<int>& nums, int target, int left, int right) {
+        if(left >= right) {
+            return ans;
         }
-        return indices;
+        cout<< nums[left] << " " << nums[right] << endl;
+        if(nums[left] + nums[right] == target) {
+            ans[0] = left + 1;
+            ans[1] = right + 1;
+            return ans;
+        } else if(nums[left] + nums[right] > target) {
+            findSum(nums, target, left, right - 1);
+        } else {
+            findSum(nums, target, left + 1, right);
+        }
+        return ans;
+    }
+
+    vector<int> twoSum(vector<int>& numbers, int target) { 
+        return findSum(numbers, target, 0, numbers.size() - 1);
     }
 };
