9ad9c14da384cd078fa3d60b2b7c54ae2c3cb225
s1834 <s18324@protonmail.com>
2024-04-11 09:57:29 +0530

    Accepted: Binary Search

--- Solved Problems/Easy/704_Binary_Search.cpp
+++ Solved Problems/Easy/704_Binary_Search.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) {
-                return i;
-            }
+    int binarySearch(vector<int>& nums, int target, int left, int right) {
+        if(left > right) {
+            return -1;
         }
+        int mid = (left + right) / 2;
+        if(nums[mid] == target) {
+            return mid;
+        } else if(nums[mid] < target) {
+            return binarySearch(nums, target, mid + 1, right);
+        } else {
+            return binarySearch(nums, target, left, mid - 1);
+        }
+    }
 
-        return -1;
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size() - 1);
     }
 };
