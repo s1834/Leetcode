edb2199775052e2041485bf942b3e82a96a2a838
s1834 <s18324@protonmail.com>
2024-06-07 00:54:50 +0530

    Accepted: didn't check what happens when map size becomes one and it still has element equal to groupSize

--- Daily Question/June 2024/2024-06-06_846_Hand_of_Straights.cpp
+++ Daily Question/June 2024/2024-06-06_846_Hand_of_Straights.cpp
@@ -1,48 +1,7 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        // int n = hand.size();
-        // if(n % groupSize != 0) {
-        //     return false;
-        // }
-
-        // map<int, int> mp;
-        // for(int i = 0; i < n; i++) {
-        //     mp[hand[i]]++;
-        // }
-
-        // bool notOne = true;
-        // while(notOne) {
-        //     notOne = false;
-        //     for (auto& x : mp) {
-        //         x.second--;
-        //         if(x.second > 1) {
-        //             notOne = true;
-        //         }
-        //     }
-        // }
-        
-        // vector<int> v;
-        // for (auto x : mp) {
-        //     if(x.second == 1) {
-        //         v.push_back(x.first);
-        //     }
-        // }
-
-        // n = v.size();
-        // if(n <= 1) {
-        //     return true;
-        // }
-
-        // for (int i = 0; i < n - 1; i++) {
-        //     if(v[i + 1] != v[i] + 1) {
-        //         return false;
-        //     }
-        // }
-
-        // return true;
-
-         int n = hand.size();
+        int n = hand.size();
         if(n % groupSize != 0) {
             return false;
         }
@@ -52,18 +11,14 @@ public:
             mp[hand[i]]++;
         }
 
-        bool notOne = true;
         int temp, prev;
-        while(notOne) {
-            notOne = false;
+        while(mp.size() != 0) {
             temp = groupSize;
-            cout<< "again\n";
             for (auto& x : mp) {
                 temp--;
                 if (temp == -1) {
                     break;
                 }
-                cout << x.first << ' ' << x.second<<endl;
                 if(temp <= groupSize - 2) {
                     if(prev + 1 != x.first) {
                         return false;
@@ -74,33 +29,15 @@ public:
                 } else {
                     x.second--;
                     prev = x.first;
-                }  
-                if(x.second >= 1) {
-                    notOne = true;
+                }
+                if(mp.size() == 1 && x.second > 0) {
+                    return 0;
                 }
                 if(x.second == 0) {
                     mp.erase(x.first);
                 } 
             }
         }
-        
-        // vector<int> v;
-        // for (auto x : mp) {
-        //     if(x.second == 1) {
-        //         v.push_back(x.first);
-        //     }
-        // }
-
-        // n = v.size();
-        // if(n <= 1) {
-        //     return true;
-        // }
-
-        // for (int i = 0; i < n - 1; i++) {
-        //     if(v[i + 1] != v[i] + 1) {
-        //         return false;
-        //     }
-        // }
 
         return true;
     }
