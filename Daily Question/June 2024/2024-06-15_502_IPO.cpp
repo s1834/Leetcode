ae5bfc73674549ee910e60bb065ed1f228b93570
s1834 <s18324@protonmail.com>
2024-06-16 02:04:42 +0530

    Accepted: needed help, and had to use priority queue

--- Daily Question/June 2024/2024-06-15_502_IPO.cpp
+++ Daily Question/June 2024/2024-06-15_502_IPO.cpp
@@ -1,59 +1,26 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        map<int,int> mp;
+        vector<pair<int, int>> project;
         int n = profits.size();
-        if (n <= k) {
-            for (int i = 0; i < n; i++) {
-                if (capital[i] <= w) {
-                    w += profits[i];
-                }
-            }
-            return w;
+        for (int i = 0; i < n; i++) {
+            project.push_back(make_pair(capital[i], profits[i]));
         }
-        for(int i = 0; i < n; i++) {
-            if(mp[capital[i]]) {
-                if(mp[capital[i]] < profits[i]) {
-                    mp[capital[i]] = profits[i];
-                }
-            } else {
-                mp[capital[i]] = profits[i];
+        sort(project.begin(), project.end());
+
+        priority_queue<int> pq;
+        int ind = 0;
+        while(k--) {
+            while(ind < n && project[ind].first <= w) {
+                pq.push(project[ind].second);
+                ind++;
             }
+            if(pq.empty()) {
+                break;
+            } 
+            w += pq.top();
+            pq.pop();
         }
-
-        vector<int> cap;
-        bool wExists = false;
-        for (auto x : mp) {
-            cap.push_back(x.first);
-            if(x.first <= w) {
-                wExists = true;
-            }
-        }
-
-        while(k && wExists) {
-            int maxW = 0, maxInd = 0;
-            wExists = false;
-            for(int i = 0; i < cap.size(); i++) {
-                if (cap[i] > w) {
-                    break;
-                }
-
-                if(mp[cap[i]] > maxW) {
-                    maxW = mp[cap[i]];
-                    maxInd = i; 
-                }
-            }
-                w += maxW;
-                cap.erase(cap.begin() + maxInd);
-                k--;
-                
-                for(int i = 0; i < cap.size(); i++) {
-                    if(cap[i] <= w) {
-                        wExists = true;
-                    }
-                }
-            }
-
         return w;
     }
 };
