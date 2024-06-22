98c954f75be844dd710114211b90380efc61cc3c
s1834 <s18324@protonmail.com>
2024-06-22 22:51:03 +0530

    Accepted: Pratyush help needed

--- Daily Question/June 2024/2024-06-22_1248_Count_Number_of_Nice_Subarrays.cpp
+++ Daily Question/June 2024/2024-06-22_1248_Count_Number_of_Nice_Subarrays.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int lastOddInd = -1, count = 0, n = nums.size();
-        bool exists = false;
-        for (int i = n - 1; i >= 0; i--) {
+        int ind = 0, count = 0, ans = 0, n = nums.size();
+        for (int i = 0; i < n; i++) {
             if(nums[i] % 2) {
-                if(lastOddInd == -1) {
-                    lastOddInd = i;
-                }
+                k--;
+                count = 0;
+            }
+            while(!k) {
+                k += (nums[ind] % 2);
                 count++;
+                ind++;
             }
-            if(count == k) {
-                exists = true;
-                break;
-            }
+            ans += count;
         }
-
-        if(!exists) {
-            return 0;
-        }
-
-        int ans = 0;
-        for (int i = 0; i < lastOddInd + 1; i++) {
-            count = 0;
-            for (int j = i; j < n; j++) {
-                if(nums[j] % 2) {
-                    count++;
-                }
-                if(count == k) {
-                    ans++;
-                }
-                if(count > k) {
-                    break;
-                }
-            }
-        }
-        return ans; 
+        return ans;
     }
 };
