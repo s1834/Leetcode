e90640c6c41e60f334b53819815901f4e8dc58f4
s1834 <s18324@protonmail.com>
2024-06-25 14:26:11 +0530

    Accepted: Learning Binary trees

---
+++ Solved Problems/Easy/226_Invert_Binary_Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) {
+            return NULL;
+        }
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        TreeNode* ptr = root->right;
+        root->right = root->left;
+        root->left = ptr;
+        return root;
+    }
+};
