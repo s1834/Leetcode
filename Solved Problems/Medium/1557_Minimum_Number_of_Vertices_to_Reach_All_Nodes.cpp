7c7b5055a14b45b546a10e57fef14510a1b8f4f8
s1834 <s18324@protonmail.com>
2024-06-29 23:37:51 +0530

    Accepted

---
+++ Solved Problems/Medium/1557_Minimum_Number_of_Vertices_to_Reach_All_Nodes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> adj[n];
+        for(auto edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+        vector<int> reached(n, 0);
+        for(auto row : adj) {
+            for(auto col : row) {
+                if(!reached[col]) reached[col] = 1;
+            }
+        }
+
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            if(!reached[i]) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
