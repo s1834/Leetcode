44e4fc5a7ae1bc917e71bbdf91250901a910b613
s1834 <s18324@protonmail.com>
2024-06-29 23:43:04 +0530

    Accepted: better solution using indegree, help from solution needed

--- Solved Problems/Medium/1557_Minimum_Number_of_Vertices_to_Reach_All_Nodes.cpp
+++ Solved Problems/Medium/1557_Minimum_Number_of_Vertices_to_Reach_All_Nodes.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int> adj[n];
+        // vector<int> adj[n];
+        // for(auto edge : edges) {
+        //     adj[edge[0]].push_back(edge[1]);
+        // }
+        // vector<int> reached(n, 0);
+        // for(auto row : adj) {
+        //     for(auto col : row) {
+        //         if(!reached[col]) reached[col] = 1;
+        //     }
+        // }
+
+        // vector<int> ans;
+        // for(int i = 0; i < n; i++) {
+        //     if(!reached[i]) ans.push_back(i);
+        // }
+
+        // return ans;
+
+        vector<int> indegree(n, 0);
         for(auto edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-        }
-        vector<int> reached(n, 0);
-        for(auto row : adj) {
-            for(auto col : row) {
-                if(!reached[col]) reached[col] = 1;
-            }
+            indegree[edge[1]] = 1;
         }
 
         vector<int> ans;
         for(int i = 0; i < n; i++) {
-            if(!reached[i]) ans.push_back(i);
+            if(!indegree[i]) ans.push_back(i);
         }
 
         return ans;
