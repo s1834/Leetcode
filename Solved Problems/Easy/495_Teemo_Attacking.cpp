b5802cfb82c9888c50c005c81dea39dde2a8c6e4
s1834 <s18324@protonmail.com>
2024-07-01 04:59:17 +0530

    Accepted: Learned about merge interval from editorial

--- Solved Problems/Easy/495_Teemo_Attacking.cpp
+++ Solved Problems/Easy/495_Teemo_Attacking.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
         int n = timeSeries.size();
-        vector<int> poisoned(timeSeries[n - 1] + duration , 0);
-        for(int i = 0; i < n; i++) {
-            for(int j = timeSeries[i]; j < timeSeries[i] + duration; j++) {
-                if(!poisoned[j]) poisoned[j] = 1;
-            }
+        if(!n) return 0;
+        int total = 0;
+        for(int i = 0; i < n - 1; i++) {
+            total += min(timeSeries[i + 1] - timeSeries[i], duration);
         }
-
-        int count = 0;
-        n = poisoned.size();
-        for(int i = 1; i < n; i++) {
-            if(poisoned[i]) count++;
-        }
-        return count;
+        return total + duration;
     }
 };
