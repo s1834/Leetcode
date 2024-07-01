2e82b4127b9bc729f935e908aaa8e7ebbdf2431e
s1834 <s18324@protonmail.com>
2024-07-01 05:40:47 +0530

    Accepted: wrong sequence of count and check

--- Daily Question/July 2024/2024-07-01_1550_Three_Consecutive_Odds.cpp
+++ Daily Question/July 2024/2024-07-01_1550_Three_Consecutive_Odds.cpp
@@ -3,9 +3,9 @@ public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int n = arr.size(), count = 0;
         for(int i = 0; i < n; i++) {
-            if(count == 3) return true;
             if(arr[i] % 2 == 0) count = 0;
             else count++;
+            if(count == 3) return true;
         }   
         return false;
     }
