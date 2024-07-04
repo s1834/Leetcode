29a21493d972f08243509c0cb98079e2ab61f38f
s1834 <s18324@protonmail.com>
2024-07-05 02:40:21 +0530

    Accepted: better time and space complexity

--- Solved Problems/Medium/56_Merge_Intervals.cpp
+++ Solved Problems/Medium/56_Merge_Intervals.cpp
@@ -1,24 +1,38 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         auto lamda = [](vector<int> v1, vector<int> v2) {
+//             return v1[0] < v2[0];
+//         };
+//         sort(intervals.begin(), intervals.end(), lamda);
+
+//         stack<vector<int>> st;
+//         st.push(intervals[0]);
+//         int n = intervals.size();
+//         for(int i = 1; i < n; i++) {
+//             if(intervals[i][0] <= st.top()[1]) st.top()[1] = max(st.top()[1], intervals[i][1]);
+//             else st.push(intervals[i]);
+//         }
+
+//         intervals.clear();
+//         while(!st.empty()) {
+//             intervals.push_back(st.top());
+//             st.pop();
+//         }
+//         return intervals;
+//     }
+// };
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        auto lamda = [](vector<int> v1, vector<int> v2) {
-            return v1[0] < v2[0];
-        };
-        sort(intervals.begin(), intervals.end(), lamda);
-
-        stack<vector<int>> st;
-        st.push(intervals[0]);
-        int n = intervals.size();
-        for(int i = 1; i < n; i++) {
-            if(intervals[i][0] <= st.top()[1]) st.top()[1] = max(st.top()[1], intervals[i][1]);
-            else st.push(intervals[i]);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(vector<int> v : intervals) {
+            if(ans.back()[1] >= v[0]) ans.back()[1] = max(ans.back()[1], v[1]);
+            else ans.push_back(v);
         }
-
-        intervals.clear();
-        while(!st.empty()) {
-            intervals.push_back(st.top());
-            st.pop();
-        }
-        return intervals;
+        return ans;
     }
 };
