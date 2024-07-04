a7eaebcfd8e06089550d95ae0192cc55fb1b9a72
s1834 <s18324@protonmail.com>
2024-07-05 03:02:06 +0530

    WA: if interval was empty

---
+++ Solved Problems/Medium/57_Insert_Interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+        bool inserted = false, resolve = false;
+        for(vector<int> v : intervals) {
+            if(resolve) {
+                ans.push_back(v);
+            } else if(inserted) {
+                if(ans.back()[1] >= v[0]) {
+                    ans.back()[1] = max(ans.back()[1], v[1]);
+                } else {
+                    ans.push_back(v);
+                    resolve = true;
+                }
+            } else {
+                if(v[1] >= newInterval[0]) {
+                    v[1] = max(v[1], newInterval[1]);
+                    inserted = true;
+                }
+                ans.push_back(v);
+            }
+        }
+        return ans;
+    }
+};
