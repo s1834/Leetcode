c7cd7282d8390f1f93f9826a48df9d94a6222894
s1834 <s18324@protonmail.com>
2024-07-06 07:29:55 +0530

    Aceepted

--- Daily Question/July 2024/2024-07-06_2582_Pass_the_Pillow.cpp
+++ Daily Question/July 2024/2024-07-06_2582_Pass_the_Pillow.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        if(n > time) return time + 1;
-        if((time / n) % 2 && time % n) return n - time / n - 1;
-        if((time / n) % 2) return n - time / n;
-        return time - n - 1;
+        return (time / (n - 1)) % 2 ? n - time % (n - 1) : time % (n - 1) + 1;
     }
 };
