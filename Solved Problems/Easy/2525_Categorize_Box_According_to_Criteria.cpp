8082174b028db1a695611395a34536eb003e0a8c
s1834 <s18324@protonmail.com>
2024-07-08 03:09:09 +0530

    Acceptd: e issue

--- Solved Problems/Easy/2525_Categorize_Box_According_to_Criteria.cpp
+++ Solved Problems/Easy/2525_Categorize_Box_According_to_Criteria.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     string categorizeBox(int length, int width, int height, int mass) {
-        bool bulky = (length >= 10e4 || width >= 10e4 || height >= 10e4 || (long long int) length * width * height >= 10e9) ? true : false, heavy = mass >= 100 ? true : false;
+        bool bulky = (length >= 1e4 || width >= 1e4 || height >= 1e4 || (long long int) length * width * height >= 1e9) ? true : false, heavy = mass >= 100 ? true : false;
         if(bulky && heavy) return "Both";
         if(!bulky && !heavy) return "Neither";
         if(bulky && !heavy) return "Bulky";
+        cout << (long long int) length * width * height;
         return "Heavy";
     }
 };
