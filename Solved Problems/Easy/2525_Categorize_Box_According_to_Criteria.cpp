127ccb8fd22793417dd574d6b54b222fe88cf1c3
s1834 <s18324@protonmail.com>
2024-07-08 03:05:00 +0530

    Runtime Error

---
+++ Solved Problems/Easy/2525_Categorize_Box_According_to_Criteria.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+        bool bulky = (length >= 10e4 || width >= 10e4 || height >= 10e4 || length * width * height >= 10e9) ? true : false, heavy = mass >= 100 ? true : false;
+        if(bulky && heavy) return "Both";
+        if(!bulky && !heavy) return "Neither";
+        if(bulky && !heavy) return "Bulky";
+        return "Heavy";
+    }
+};
