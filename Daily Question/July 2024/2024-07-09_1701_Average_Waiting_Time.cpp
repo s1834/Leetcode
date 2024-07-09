1dcbb0ef187a9f45c01d967741214779233c1d4c
s1834 <s18324@protonmail.com>
2024-07-09 18:03:58 +0530

    Accepted: issue in logic

--- Daily Question/July 2024/2024-07-09_1701_Average_Waiting_Time.cpp
+++ Daily Question/July 2024/2024-07-09_1701_Average_Waiting_Time.cpp
@@ -4,8 +4,13 @@ public:
         int waitingTime = customers[0][0] + customers[0][1], n = customers.size();
         double averageWaitingTime = customers[0][1];
         for(int i = 1; i < n; i++) {
-            waitingTime += customers[i][1];
-            averageWaitingTime += customers[i][0] > waitingTime ? customers[i][1] : waitingTime - customers[i][0];
+           if(waitingTime > customers[i][0]) {
+                waitingTime += customers[i][1];
+                averageWaitingTime += waitingTime - customers[i][0];
+            } else {
+                waitingTime = customers[i][0] + customers[i][1];
+                averageWaitingTime += customers[i][1];
+            }
         }
         return averageWaitingTime / n; 
     }
