33eeb33c4e3bff04a28cdc244d17607f829d2ac1
s1834 <s18324@protonmail.com>
2024-07-24 04:16:19 +0530

    Accepted: using 2 pointer

--- Solved Problems/Easy/125_Valid_Palindrome.cpp
+++ Solved Problems/Easy/125_Valid_Palindrome.cpp
@@ -1,21 +1,38 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+//         vector<char> ch;
+//         int size = s.size(),temp = 0;
+
+//         for (int i = 0; i < size; i++) {
+//             if (isalnum(s[i])) {
+//                 ch.push_back(tolower(s[i]));
+//             }
+//         }
+//         size = ch.size();
+//         temp = size - 1;
+//         for (int i = 0; i < size / 2; i++) {
+//             if (ch[i] != ch[temp]) {
+//                 return false;
+//             }
+//             temp--;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char> ch;
-        int size = s.size(),temp = 0;
-
-        for (int i = 0; i < size; i++) {
-            if (isalnum(s[i])) {
-                ch.push_back(tolower(s[i]));
+        int n = s.size(), first = 0, last = n - 1;
+        while(first < last) {
+            if(!isalnum(s[first])) first++;
+            else if(!isalnum(s[last])) last--;
+            else {
+                if(tolower(s[first]) != tolower(s[last])) return false;
+                first++;
+                last--;
             }
-        }
-        size = ch.size();
-        temp = size - 1;
-        for (int i = 0; i < size / 2; i++) {
-            if (ch[i] != ch[temp]) {
-                return false;
-            }
-            temp--;
         }
         return true;
     }
