1161b6241f94b6974e1b6fe081c4e06e38dc7a4d
s1834 <s18324@protonmail.com>
2024-09-03 21:31:14 +0530

    WA: dont overcomplicate stuff, keep it simple and think of all possibilities

--- Solved Problems/Medium/3_Longest_Substring_Without_Repeating_Characters.cpp
+++ Solved Problems/Medium/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    bool isDistinct(string s, int i, int j) {
-        vector<bool> visited(256);
-        for(int k = i; k <= j; k++) {
-            if(visited[s[k]]) {
-                return false;
-            }
-            visited[s[k]] = true;
-        }
-        return true;
-    }
-
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int len = 0;
-        for (int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(isDistinct(s, i, j)) {
-                    len = max(len, j - i + 1);
-                }
+        map<char, int> mp;
+        int longest = 0;
+        for(auto x : s) {
+            mp[x]++;
+            if(mp[x] > 1) {
+                if(mp.size() > longest) longest = mp.size();
+                mp.clear();
+                mp[x]++;
             }
         }
-        return len;
+        return longest;
     }
 };
