0d511bbff2f231aae07ab0cd62867d94229702e8
s1834 <s18324@protonmail.com>
2025-03-09 00:37:04 +0530

    Accepted: Checked for if n == k

--- Daily Question/2025/March 2025/2025-03-08_2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
+++ Daily Question/2025/March 2025/2025-03-08_2379_Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.cpp
@@ -2,13 +2,13 @@ class Solution {
     public:
         int minimumRecolors(string blocks, int k) {
             int n = blocks.size(), recolor, recolorMin = INT_MAX;
-            for(int i = 0; i < n - k; i++) {
+            for(int i = 0; i <= n - k; i++) {
                 recolor = 0;
                 for(int j = i; j < i + k; j++) {
                     if(blocks[j] == 'W') recolor++;
                 }
                 if(recolorMin > recolor) recolorMin = recolor;
             }
-            return recolorMin;
+            return recolorMin == INT_MAX ? recolor : recolorMin;
         }
     };
