3eeec6a0c6d23974d7dcf639880a2909d77806fc
s1834 <s18324@protonmail.com>
2025-03-24 14:57:38 +0530

    Accepted: forgot to sort

--- Solved Problems/Medium/2410_Maximum_Matching_of_Players_With_Trainers.cpp
+++ Solved Problems/Medium/2410_Maximum_Matching_of_Players_With_Trainers.cpp
@@ -2,6 +2,8 @@ class Solution {
     public:
         int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
             int i = 0, j = 0, n = players.size(), m = trainers.size(), count = 0;
+            sort(players.begin(), players.end());
+            sort(trainers.begin(), trainers.end());
             while(i < n && j < m) {
                 if(players[i] <= trainers[j]) {
                     count++;
