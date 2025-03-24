d46b22876a9e00ff667666f7115906b2d507def0
s1834 <s18324@protonmail.com>
2025-03-24 14:47:54 +0530

    TLE

---
+++ Solved Problems/Easy/455_Assign_Cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int findContentChildren(vector<int>& g, vector<int>& s) {
+            int i = 0, j = 0, n = g.size(), m = s.size(), count = 0;
+            sort(g.begin(), g.end());
+            sort(s.begin(), s.end());
+            while(i < n && j < m) {
+                if(g[i] == s[j]) {
+                    count++;
+                    i++;
+                    j++;
+                } else if(g[i] > s[j]) j++;
+            }
+            return count;
+        }
+    };
