a4de44f3e014f6c62bc9e4f990094cf122955124
s1834 <s18324@protonmail.com>
2025-03-26 01:03:29 +0530

    WA

--- Solved Problems/Easy/2200_Find_All_K-Distant_Indices_in_an_Array.cpp
+++ Solved Problems/Easy/2200_Find_All_K-Distant_Indices_in_an_Array.cpp
@@ -3,13 +3,17 @@ class Solution {
         vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
             set<int> s;
             int n = nums.size(), i = 0, j = 0;
-            while(j < n) {
+            while(j < n & i < n) {
                 if(abs(i - j) <= k) {
-                    if(nums[i] == key || nums[j] == key) {
+                    if(nums[i] == key) {
                          s.insert(i);
                          s.insert(j);
-                    }
-                    j++;
+                         j++;
+                    } else if(nums[j] == key) {
+                         s.insert(i);
+                         s.insert(j);
+                         i++;
+                    } else j++;
                 } else i++;
             }
             return vector<int>(s.begin(), s.end());
