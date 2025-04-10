aee6c9173c82daaa903e381b4e08b806a8ea1de0
s1834 <s18324@protonmail.com>
2025-04-11 01:58:07 +0530

    Accepted: needed to use mathematical approach as storing in vector will take a lot of space when larger numbers like 100000000 comes

--- Solved Problems/Medium/390_Elimination_Game.cpp
+++ Solved Problems/Medium/390_Elimination_Game.cpp
@@ -1,27 +1,13 @@
 class Solution {
-    private:
-        int left(vector<int> v) {
-            if(v.size() == 1) return v[0];
-            vector<int> temp;
-            int n = v.size();
-            for(int i = 1; i < n; i += 2) temp.push_back(v[i]);
-            return right(temp);
-        }
-    
-        int right(vector<int> v) {
-            if(v.size() == 1) return v[0];
-            vector<int> temp;
-            int n = v.size();
-            for(int i = n - 2; i >= 0; i -= 2) temp.push_back(v[i]);
-            reverse(temp.begin(), temp.end());
-            return left(temp);
-        }
-    
     public:
         int lastRemaining(int n) {
-            if(n == 1) return 1;
-            vector<int> v;
-            for(int i = 2; i <= n; i += 2) v.push_back(i);
-            return right(v);
+            int head = 1, step = 1, left = 1;
+            while(n > 1) {
+                if(left || n % 2) head += step;
+                step *= 2;
+                n /= 2;
+                left = !left;
+            }
+            return head;
         }
     };
