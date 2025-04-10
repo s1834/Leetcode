16853dc8f5d0a678975c74993e3196d499aacbc1
s1834 <s18324@protonmail.com>
2025-04-10 23:43:37 +0530

    TLE

---
+++ Solved Problems/Hard/3036_Number_of_Subarrays_That_Match_a_Pattern_II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    public:
+        int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
+             int n = nums.size(), m = pattern.size(), count = 0;
+            bool flag;
+            for(int i = 0; i < n - m; i++) {
+                for(int j = 0; j < m; j++) {
+                    flag = true;
+                    if(pattern[j] == 1) {
+                        if(nums[i + j + 1] <= nums[i + j]) {
+                            flag = false;
+                            break;
+                        }
+                    } else if(pattern[j] == -1) {
+                        if(nums[i + j + 1] >= nums[i + j]) {
+                            flag = false;
+                            break;
+                        }
+                    } else {
+                        if(nums[i + j + 1] != nums[i + j]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                }
+                if(flag) count++;
+            }
+            return count;
+        }
+    };
