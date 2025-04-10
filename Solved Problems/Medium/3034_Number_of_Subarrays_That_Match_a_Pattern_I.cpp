b5737e36345f759323c4ec511545915d0eeb30bc
s1834 <s18324@protonmail.com>
2025-04-10 23:40:29 +0530

    Accepted: if condition error before

--- Solved Problems/Medium/3034_Number_of_Subarrays_That_Match_a_Pattern_I.cpp
+++ Solved Problems/Medium/3034_Number_of_Subarrays_That_Match_a_Pattern_I.cpp
@@ -2,16 +2,17 @@ class Solution {
     public:
         int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
             int n = nums.size(), m = pattern.size(), count = 0;
+            bool flag;
             for(int i = 0; i < n - m; i++) {
-                bool flag = true;
                 for(int j = 0; j < m; j++) {
+                    flag = true;
                     if(pattern[j] == 1) {
-                        if(nums[i + j + 1] < nums[i + j]) {
+                        if(nums[i + j + 1] <= nums[i + j]) {
                             flag = false;
                             break;
                         }
                     } else if(pattern[j] == -1) {
-                        if(nums[i + j + 1] > nums[i + j]) {
+                        if(nums[i + j + 1] >= nums[i + j]) {
                             flag = false;
                             break;
                         }
