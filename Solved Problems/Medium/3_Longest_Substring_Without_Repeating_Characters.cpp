09693a24d61fdf6b0203492345644d3f9f07c140
s1834 <s18324@protonmail.com>
2025-06-30 01:47:44 +0530

    first create a set; keep finding and erasing if common found and find max; if no common found keep increasing count and return the max

--- Solved Problems/Medium/3_Longest_Substring_Without_Repeating_Characters.cpp
+++ Solved Problems/Medium/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mp;
-        int longest = 0, left = 0, right = 0, n = s.size();
-        while(left <= right && right < n) {
-            mp[s[right]]++;
-            while(mp[s[right]] > 1) {
-                mp[s[left]]--;
-                if(!mp[s[left]]) mp.erase(s[left]);
-                left++;
+        set<char> st;
+        int l = 0, r = 0, n = s.size(), mx = 0, count = 0;
+        while(r < n) {
+            while(st.find(s[r]) != st.end()) {
+                st.erase(s[l]);
+                l++;
+                mx = max(mx, count);
+                count--;
             }
-            longest = max(longest, right - left + 1);
-            right++;
+            st.insert(s[r]);
+            count++;
+            r++;
         }
-        
-        return longest;
+        return max(mx, count);
     }
 };
+
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         unordered_map<char, int> mp;
+//         int longest = 0, left = 0, right = 0, n = s.size();
+//         while(left <= right && right < n) {
+//             mp[s[right]]++;
+//             while(mp[s[right]] > 1) {
+//                 mp[s[left]]--;
+//                 if(!mp[s[left]]) mp.erase(s[left]);
+//                 left++;
+//             }
+//             longest = max(longest, right - left + 1);
+//             right++;
+//         }
+        
+//         return longest;
+//     }
+// };
