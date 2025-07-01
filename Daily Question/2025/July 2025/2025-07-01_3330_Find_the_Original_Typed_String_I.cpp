927d7ee7e3fd05200b61ac8d2042f823c095505a
s1834 <s18324@protonmail.com>
2025-07-01 15:38:27 +0530

    Accepted: just count repeating characters remember cases like abbccfscc

---
+++ Daily Question/2025/July 2025/2025-07-01_3330_Find_the_Original_Typed_String_I.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int count = 0, n = word.size(), ans = 0;
+        for(int i = 1; i < n; i++) {
+            if(word[i] != word[i - 1]) {
+                if(count > 0) ans += count;
+                count = 0;
+            } else count++;
+        }
+        return ans + count + 1;
+    }
+};
