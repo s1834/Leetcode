6cf5b050d6d88333b7cb18ffa5f7e7fe636c21ed
s1834 <s18324@protonmail.com>
2025-07-03 15:14:57 +0530

    Accepted

--- Daily Question/2025/July 2025/2025-07-03_3304_Find_K-th_Charater_in_String_Game_I.cpp
+++ Daily Question/2025/July 2025/2025-07-03_3304_Find_K-th_Charater_in_String_Game_I.cpp
@@ -2,7 +2,18 @@ class Solution {
 public:
     char kthCharacter(int k) {
         string word = "a";
-        while(word.size() < k) for(auto x : word) word += x + 1;
+        while(word.size() < k) {
+            string temp = "";
+            for(auto x : word) temp += x + 1;
+            word += temp;
+        }
         return word[k - 1];
     }
 };
+
+/*
+1. keep checking the size of string and add chars until it is at least k
+2. then return the k-1 th character
+
+the error in last submission was i was directly increasing the size of the word, and as word was already in x, it gave runtime error
+*/
