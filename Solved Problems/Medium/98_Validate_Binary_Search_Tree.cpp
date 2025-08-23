a5462f6ed512766b1bc3e7c236afae23717bb0f4
s1834 <s18324@protonmail.com>
2025-08-24 01:42:10 +0530

    Accepted

--- Solved Problems/Medium/98_Validate_Binary_Search_Tree.cpp
+++ Solved Problems/Medium/98_Validate_Binary_Search_Tree.cpp
@@ -10,10 +10,24 @@
  * };
  */
 class Solution {
+private:
+    bool isValid(TreeNode* root, long long int mn, long long int mx) {
+        if(!root) return true;
+        if(root->val < mx && root->val > mn) return isValid(root->left, mn, root->val) && isValid(root->right, root->val, mx);
+        return false;
+    }
+
 public:
     bool isValidBST(TreeNode* root) {
-        if(!root) return true;
-        if((root->left && root->val <= root->left->val) || (root->right && root->val >= root->right->val)) return false;
-        return isValidBST(root->left) && isValidBST(root->right);
+        return isValid(root, LLONG_MIN, LLONG_MAX);
     }
 };
+
+/*
+1) The isValid function checks if the current node's value is within the valid range (mn, mx).
+2) If the current node is null, it means we've reached a leaf node, and we return true.
+3) If the current node's value is not within the valid range, we return false.
+4) We recursively check the left and right subtrees with updated ranges.
+   - For the left subtree, the maximum value is updated to the current node's value.
+   - For the right subtree, the minimum value is updated to the current node's value.
+*/
