2b79fa1e7e8be3403c1ec35e17114735ac75c441
s1834 <s18324@protonmail.com>
2025-08-24 01:47:13 +0530

    Accepted: Added another soltion from flam interview

--- Solved Problems/Hard/76_Minimum_Window_Substring.cpp
+++ Solved Problems/Hard/76_Minimum_Window_Substring.cpp
@@ -1,38 +1,73 @@
+// class Solution {
+// public:
+//     string minWindow(string s, string t) {
+//         map<char, int> mp1, mp2;
+//         for (auto x : t) mp1[x]++;
+        
+//         int n = s.size(), l = 0, r = 0, mn = INT_MAX, minL = -1, minR = -1;
+//         int have = 0, need = mp1.size();
+//         while(r < n) {
+//             if(mp1[s[r]]) mp2[s[r]]++;
+
+//             if(mp1[s[r]] && mp1[s[r]] == mp2[s[r]]) have++;
+
+//             while(have == need) {
+//                 if(r - l + 1 < mn) {
+//                     minL = l;
+//                     minR = r;
+//                     mn = r - l + 1;
+//                 }
+
+//                 mp2[s[l]]--;
+//                 if(mp1[s[l]] && mp2[s[l]] < mp1[s[l]]) have--;
+//                 l++;
+//             }
+//             r++;
+//         }
+
+//         string ans = "";
+//         for(int i = minL; i <= minR && mn != INT_MAX; i++) ans += s[i];
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char, int> mp1, mp2;
-        for (auto x : t) mp1[x]++;
-        
-        int n = s.size(), l = 0, r = 0, mn = INT_MAX, minL = -1, minR = -1;
-        int have = 0, need = mp1.size();
-        while(r < n) {
-            if(mp1[s[r]]) mp2[s[r]]++;
+        int n = s.size(), m = t.size();
+        if (n < m) return "";
 
-            if(mp1[s[r]] && mp1[s[r]] == mp2[s[r]]) have++;
+        unordered_map<char, int> mp1, mp2;
+        for (char c : t) mp1[c]++;
 
-            while(have == need) {
-                if(r - l + 1 < mn) {
-                    minL = l;
-                    minR = r;
-                    mn = r - l + 1;
+        int l = 0, r = 0, have = 0, need = mp1.size(), first = 0, last = INT_MAX;
+
+        while (r < n) {
+            mp2[s[r]]++;
+
+            if (mp1[s[r]] && mp2[s[r]] == mp1[s[r]]) have++;
+
+            while (have == need) {
+                if (r - l + 1 < last) {
+                    last = r - l + 1;
+                    first = l;
                 }
 
                 mp2[s[l]]--;
-                if(mp1[s[l]] && mp2[s[l]] < mp1[s[l]]) have--;
+                if (mp1[s[l]] && mp2[s[l]] < mp1[s[l]]) have--;
                 l++;
             }
             r++;
         }
 
-        string ans = "";
-        for(int i = minL; i <= minR && mn != INT_MAX; i++) ans += s[i];
-        return ans;
+        if (last == INT_MAX) return "";
+        return s.substr(first, last);
     }
 };
 
 
 /*
+Explanation of commented code:
 1) map all the charaters of t in mp1
 2) use two pointers l and r to traverse the string s
 3) use mp2 to keep track of the characters in the current window
